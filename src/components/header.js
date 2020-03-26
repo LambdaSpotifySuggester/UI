17bbeef216d0e902a7e1a035083fcd06ae6dd651
JessBonanno <DerpDerpRun@users.noreply.github.com>
2020-03-25 21:53:10 -0400

    changed fonts added an icon to browser tab

--- src/components/header.js
+++ src/components/header.js
@@ -4,8 +4,8 @@ function Header() {
     return (
       <header className='header'>          
           <div className='text-wrapper'>
-              <h2>Spotify Suggestor</h2>
-              <h3>Isolation Recommendations</h3>
+              <h2>Isolation Recommendations</h2>
+              <h3>A Spotify Song Suggestor</h3>
           </div>
           <button className='start-btn'>Get Started</button>
       </header>  
